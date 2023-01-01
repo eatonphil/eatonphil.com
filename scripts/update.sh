23156be37bdb03764aee979f2b3e281f8c3a0f64
Phil Eaton <phil@eatonphil.com>
2023-01-01 16:48:42 -0500

    Update update

--- scripts/update.sh
+++ scripts/update.sh
@@ -5,6 +5,7 @@ set -eu
 cd ~/eatonphil.com
 git reset --hard
 git pull
+sudo cp Caddyfile /etc/caddy/Caddyfile
 sudo rm -rf /usr/share/caddy/*
 sudo mv {home,lists,notes} /usr/share/caddy/
 sudo chown -R caddy:caddy /usr/share/caddy/
