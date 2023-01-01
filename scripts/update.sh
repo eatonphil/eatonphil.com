14863e1949298551fefb7b13e28e92bd0e7e0772
Phil Eaton <phil@eatonphil.com>
2023-01-01 16:46:15 -0500

    Move folders only

--- scripts/update.sh
+++ scripts/update.sh
@@ -6,6 +6,6 @@ cd ~/eatonphil.com
 git reset --hard
 git pull
 sudo rm -rf /usr/share/caddy/*
-sudo mv * /usr/share/caddy/
+sudo mv {home,lists,notes} /usr/share/caddy/
 sudo chown -R caddy:caddy /usr/share/caddy/
 sudo restorecon -r /usr/share/caddy/
