33d8c3f303788a6e505a9986151f2e36db04d445
Phil Eaton <phil@eatonphil.com>
2023-01-02 11:52:00 -0500

    Update update script for new domain

--- scripts/update.sh
+++ scripts/update.sh
@@ -7,7 +7,7 @@ git reset --hard
 git pull
 sudo cp Caddyfile /etc/caddy/Caddyfile
 sudo rm -rf /usr/share/caddy/*
-sudo mv {home,lists,notes} /usr/share/caddy/
+sudo mv {home,lists,notes,letters} /usr/share/caddy/
 sudo chown -R caddy:caddy /usr/share/caddy/
 sudo restorecon -r /usr/share/caddy/
 sudo service caddy restart
