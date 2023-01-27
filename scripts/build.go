b464864e59686a76e54da8af823c5371bb88d406
Phil Eaton <phil@eatonphil.com>
2023-01-26 21:25:37 -0500

    Builder seems to be working!

--- scripts/build.go
+++ scripts/build.go
@@ -7,8 +7,28 @@ import (
 	"strings"
 	"bufio"
 	"bytes"
-	"html/template"
+	"text/template"
 )
+
+func copyFile(in, out string) {
+	r, err := os.Open(in)
+	if err != nil {
+		panic(err)
+	}
+	defer r.Close()
+
+	w, err := os.Create(out)
+	if err != nil {
+		panic(err)
+	}
+	defer w.Close()
+
+	// TODO: validate complete read/write
+	_, err = w.ReadFrom(r)
+	if err != nil {
+		panic(err)
+	}
+}
 
 var SECTIONS = []struct{
 	name string
@@ -16,19 +36,31 @@ var SECTIONS = []struct{
 }{
 	{
 		"notes",
-		map[string]any{"Tag": "Notes by a software developer"},
+		map[string]any{
+			"Tag": "Notes by a software developer",
+			"Section": "Notes",
+		},
 	},
 	{
 		"letters",
-		map[string]any{"Tag": "Letters by a software developer"},
+		map[string]any{
+			"Tag": "Letters by a software developer",
+			"Section": "Letters",
+		},
 	},
 	{
 		"lists",
-		map[string]any{"Tag": "Phil Eaton's Lists"},
+		map[string]any{
+			"Tag": "Phil Eaton's Lists",
+			"Section": "Lists",
+		},
 	},
 	{
 		"home",
-		map[string]any{"Tag": "Phil Eaton"},
+		map[string]any{
+			"Tag": "Phil Eaton",
+			"Section": "",
+		},
 	},
 }
 
@@ -36,8 +68,8 @@ type Doc struct {
 	Title string
 	Date string
 	Tags []string
-	Body []byte
-	FullUrl string
+	Body string
+	CanonicalURL string
 }
 
 func parseDoc(docPath string) Doc {
@@ -96,14 +128,15 @@ func parseDoc(docPath string) Doc {
 				c = bodyRunes[i]
 			}
 
-			fmt.Fprintf(outWriter, "<h%d>%s</hd%>", headerNumber, header, headerNumber)
+			fmt.Fprintf(outWriter, "<h%d>%s</h%d>", headerNumber, string(header), headerNumber)
 			continue
 		}
 
 		outWriter.WriteRune(c)
 		i++
 	}
-	d.Body = out.Bytes()
+	outWriter.Flush()
+	d.Body = string(out.Bytes())
 
 	return d
 }
@@ -124,13 +157,27 @@ func buildSection(t *template.Template, section string, ctx map[string]any) {
 			outputPath = outputPath[:len(outputPath)-4] + ".html"
 		}
 
-		o, err := os.Open(outputPath)
+		o, err := os.Create(outputPath)
 		if err != nil  {
 			panic(err)
 		}
 
+		canonicalEnd := "/"
+		if f.Name() != "index.html" {
+			canonicalEnd = "/" + f.Name()
+		}
+		doc.CanonicalURL = ctx["Domain"].(string) + canonicalEnd
+				
 		ctx["Page"] = doc
-		t.Execute(o, ctx)
+		err = t.Execute(o, ctx)
+		if err != nil {
+			panic(err)
+		}
+
+		err = o.Close()
+		if err != nil {
+			panic(err)
+		}
 	}
 }
 
@@ -140,7 +187,12 @@ func main() {
 		panic(err)
 	}
 
-	t, err := template.New("template").ParseFiles("template.html")
+	templateFile, err := os.ReadFile("template.html")
+	if err != nil {
+		panic(err)
+	}
+
+	t, err := template.New("template").Parse(string(templateFile))
 	if err != nil {
 		panic(err)
 	}
@@ -150,18 +202,33 @@ func main() {
 			continue
 		}
 
-		buildPath := path.Join(os.Cwd(), section.name, "build")
+		if _, err := os.Stat(section.name); err != nil {
+			if os.IsNotExist(err) {
+				panic("Section does not exist:" + section.name)
+			}
+		}
+
+		// TODO: validate the section exists?
+		buildPath := path.Join(section.name, "build")
 		err := os.RemoveAll(buildPath)
 		if err != nil {
 			panic(err)
 		}
 
-		err := os.MkdirAll(buildPath, os.ModePerm)
+		err = os.MkdirAll(buildPath, os.ModePerm)
 		if err != nil {
 			panic(err)
 		}
 
-		section.ctx["mail"] = mailFile
+		// Copy in style.css
+		copyFile("style.css", path.Join(buildPath, "style.css"))
+
+		// Render all templates
+		section.ctx["Mail"] = string(mailFile)
+		section.ctx["Domain"] = "eatonphil.com"
+		if section.name != "home" {
+			section.ctx["Domain"] = section.name + ".eatonphil.com"
+		}
 		buildSection(t, section.name, section.ctx)
 	}
 }
